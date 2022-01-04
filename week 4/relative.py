6c9e2452d39d0729f566b2793d8cee091c19b241
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-01-04 14:35:03 +0300

    Add files via upload

---
+++ week 4/relative.py
@@ -0,0 +1,16 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        count = Counter(arr1)
+        relArray = []
+        temp = []
+        for i in arr2:
+            if i in count:
+                relArray.extend([i]*count[i])
+                count.pop(i)
+                
+        for key in count:
+            temp.extend([key]*count[key])
+            
+        relArray.extend(sorted(temp))
+
+        return relArray
