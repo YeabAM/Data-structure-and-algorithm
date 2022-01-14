a0843e7289f0f06f096bddf342cd5052614694b6
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-01-14 16:47:48 +0300

    Create reverse-polist.py

---
+++ week-4/reverse-polist.py
@@ -0,0 +1,24 @@
+Red, [1/13/22 6:22 PM]
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for i in range(len(tokens)):
+            if(tokens[i] == "+" ):
+                num1 = int(stack.pop())
+                num2 = int(stack.pop())
+                stack.append(num1 + num2)
+            elif(tokens[i] == "*" ):
+                num1 = int(stack.pop())
+                num2 = int(stack.pop())
+                stack.append(num1 * num2)
+            elif(tokens[i] == "/" ):
+                num1 = int(stack.pop())
+                num2 = int(stack.pop())
+                stack.append(num2 / num1)
+            elif(tokens[i] == "-" ):
+                num1 = int(stack.pop())
+                num2 = int(stack.pop())
+                stack.append(num2 - num1)
+            else:
+                stack.append(tokens[i])
+        return int(stack[0])
