b1c83fdcf967bf3d5afa3113c7303b63ae4bfb49
YeabAM <yeabkalu@gmail.com>
2022-01-22 10:21:59 +0300

    improved with monothonic stack

--- week-5/next-greater.py
+++ week-5/next-greater.py
@@ -1,14 +1,17 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        res = []
-        for i in range(len(nums1)):
-            index = nums2.index(nums1[i])
-            for j in range(index, len(nums2)):
-                temp = -2
-                if j < len(nums2)-1 and nums2[index] < nums2[j+1]:
-                    temp = nums2[j+1]
-                    break
-                else:
-                    temp = -1
-            res.append(temp)
-        return res
+        greater_dict = {}
+
+        stack = [nums2[0]]
+        
+
+        for i in range(1, len(nums2)):
+            # when stack not empty and current number greater than top of stack
+            while stack and (nums2[i] > stack[-1]):
+                greater_dict[stack.pop()] = nums2[i]
+            stack.append(nums2[i])
+
+        while stack:
+            greater_dict[stack.pop()] = -1
+
+        return [greater_dict[i] for i in nums1]
