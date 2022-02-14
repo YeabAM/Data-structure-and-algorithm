1644f36897e6b6ba06bf6bdece6656c29efb5b80
YeabAM <yeabkalu@gmail.com>
2022-02-14 22:45:32 +0300

    subarray equal to k

---
+++ week-8/subarray-equal-to-k.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        psum = 0
+        count = 0
+        psumdict = defaultdict(int)
+        
+        for i in range(len(nums)):
+            psum += nums[i]
+            
+            if psum == k:
+                count += 1
+            if (psum - k) in psumdict:
+                count += psumdict[psum - k]
+            
+            psumdict[psum] += 1
+            
+        return count
+                
+        
