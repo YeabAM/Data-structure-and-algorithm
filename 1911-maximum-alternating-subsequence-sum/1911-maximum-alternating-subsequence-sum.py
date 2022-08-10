8708b63ebab48f7423dad31d56d7994f6a2b1a6a
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-08-10 11:23:24 +0300

    Time: 4691 ms (14.62%), Space: 381.6 MB (26.83%) - LeetHub

---
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxAlternatingSum(self, nums: List[int]) -> int:
+        #check the dp pattern google sheet
+        dp = {}
+        
+        def dfs(i, even):
+            if i == len(nums):
+                return 0
+            
+            if (i, even) in dp:
+                return dp[(i, even)]
+            
+            total = nums[i] if even else (-1 * nums[i])
+            
+            dp[(i, even)] = max(total + dfs(i+1, not even), dfs(i+1, even))
+            
+            return dp[(i, even)]
+        
+        return dfs(0, True)
+            
