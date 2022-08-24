8b67caa67811aa8d1952e386cc6b69e63324c4fe
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-08-24 17:50:54 +0300

    Time: 18327 ms (5.00%), Space: 30.1 MB (5.04%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -2,21 +2,16 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return 0
-        
-        count = 1
-        jump = nums[0]
-        _max = 0
-        for i in range(1, len(nums)):
-            if jump == 0:
-                jump += _max
-                count += 1
-                _max = 0
-            jump -= 1
-            
-            if jump + i >= len(nums) - 1:
-                break
-
-            if nums[i] - jump > _max:
-                _max = nums[i] - jump   
-                
-        return count
+        memo = {}
+        def dp(idx):
+            if idx in memo:
+                return memo[idx]
+            if idx + nums[idx] >= len(nums) - 1 or idx == len(nums) - 1:
+                return 1
+            _min = float('inf')
+            for i in range(1, nums[idx]+1):
+                if i + idx < len(nums) - 1:
+                    _min = min(_min, dp(i + idx) + 1)
+            memo[idx] = _min
+            return _min
+        return dp(0)
