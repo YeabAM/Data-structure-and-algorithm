26516eeebc84fd33771a55eb84134a5970cff8fd
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-09-25 18:06:06 +0300

    Time: 506 ms (26.10%), Space: 22.4 MB (24.27%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,22 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        forward = [nums[0]]
-        backward = [nums[-1]]
+        forward = [0 for _ in range(len(nums) + 1)]
+        backward = [0 for _ in range(len(nums))]
+        forward[0] = 1
+        backward[-1] = nums[len(nums) - 1]
         result = []
-        for i in range (1, len(nums)):
-            forward.append(nums[i] * forward[i-1])
-            
-        for i in range(len(nums) - 2,-1,-1):
-            backward.append(nums[i] * backward[-1])
-            
-        backward = backward[::-1]
         
-        result.append(backward[1])
-        
-        for i in range(1, len(nums) - 1):
-            result.append(forward[i - 1] * backward[i + 1])
+        for i in range(1, len(nums)+1):
+            forward[i] = forward[i-1] * nums[i-1]
             
-        result.append(forward[-2])
+        for i in range(len(nums)-2, -1, -1):
+            backward[i] = backward[i+1] * nums[i]
+            
+        backward.append(1)   
+        # print(forward, backward)
         
+        for i in range(1, len(forward)):
+            result.append(forward[i-1] * backward[i])
+            
         return result
+            
         
