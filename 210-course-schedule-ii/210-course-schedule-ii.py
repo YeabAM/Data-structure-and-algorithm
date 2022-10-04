2d37f912b52130de8da378551441478353889213
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-10-04 11:52:01 +0300

    Time: 200 ms (45.34%), Space: 15.6 MB (71.37%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,29 +1,36 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        income = dict()
-        preqs = defaultdict(list)
-        queue = deque()
+        amountOfPreq = defaultdict(int)
+        PreqFor = defaultdict(set)
         courses = []
-        for c in range(numCourses):
-            income[c] = 0
-
-        for cour, pre in prerequisites:
-            preqs[pre].append(cour)
-            income[cour] += 1
-
-        for cour in income:
-            if income[cour] == 0:
-                queue.append(cour)
-
+        queue = deque()
+        
+        
+        for i in range(numCourses):
+            amountOfPreq[i] = 0
+            
+        for course, preqCourse in prerequisites:
+            PreqFor[preqCourse].add(course)
+            amountOfPreq[course] += 1
+            
+        for i in range(numCourses):
+            if amountOfPreq[i] == 0:
+                queue.append(i)
+                
         while queue:
-            curr = queue.popleft()
-            courses.append(curr)
-            for cour in preqs[curr]:
-                income[cour] -= 1
-                if income[cour] == 0:
-                    queue.append(cour)
-
-        if len(income) == len(courses):
-            return courses
-        return []
+            currCourse = queue.popleft()
+            courses.append(currCourse)
+            
+            for course in PreqFor[currCourse]:
+                amountOfPreq[course] -= 1
+                if amountOfPreq[course] == 0:
+                    queue.append(course)
+                    
+        return courses if len(courses) == numCourses else []
+                
+            
+        
+            
+        
+        
         
