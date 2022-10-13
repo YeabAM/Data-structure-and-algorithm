6b0627474fe599ecd992e137d3e156b336d59c78
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-10-13 10:29:05 +0300

    Time: 377 ms (5.76%), Space: 16.9 MB (51.43%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
@@ -9,20 +9,19 @@ class Solution:
         newNode = TreeNode(val)
         
         curr = root
-        if not root:
-            root = newNode
+        
         
         while curr and curr.val != val:
             if curr.val > val:
                 if not curr.left:
                     curr.left = newNode
-                    break
+                    
                 curr = curr.left
             else:
                 if not curr.right:
                     curr.right = newNode
-                    break
+                    
                 curr = curr.right
                 
-        return root
+        return root or newNode
         
