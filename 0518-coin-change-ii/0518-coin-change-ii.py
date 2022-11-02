15be6e552e1883bd7b838ba7a3057dcbae2fe17b
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-11-02 19:52:11 +0300

    Time: 550 ms (55.70%), Space: 29.8 MB (33.87%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [[0 for _ in range(amount+1)] for _ in range(len(coins) + 1)]
+        
+        dp[0][0] = 1
+        
+        for r in range(1, len(dp)):
+            for c in range(len(dp[0])):
+                prevSum = c - coins[r-1]
+                dp[r][c] = dp[r-1][c]
+                if  prevSum >= 0:
+                    dp[r][c] += dp[r][prevSum]
+                    
+        return dp[len(coins)][amount]
