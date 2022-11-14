7465ae135526baa36e1e0229a98d674576d584a1
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-11-14 14:40:27 +0300

    Time: 543 ms (18.92%), Space: 17.6 MB (60.59%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,41 +1,33 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        dp = [[1 for _ in range(len(word1) + 1)] for _ in range (len(word2) + 1)]
         
-        def edit(w1, w2):
-            if (w1, w2) in memo:
-                return memo[(w1, w2)]
-            
-            if w1 == len(word1) and w2 == len(word2):
-                memo[(w1, w2)] = 0
-                return 0
-            
-            if w1 == len(word1):
-                memo[(w1, w2)] = len(word2) - w2
-                return memo[(w1, w2)]
-            
-            if w2 == len(word2):
-                memo[(w1, w2)] = len(word1) - w1
-                return memo[(w1, w2)]
-            
-            if word1[w1] == word2[w2]:
-                memo[(w1, w2)] = edit(w1 + 1, w2 + 1)
-                return memo[(w1, w2)]
-            
-            insert = delete = replace = 0
-            if word1[w1] != word2[w2]:
-                insert = edit(w1, w2 + 1)
-                delete = edit(w1 + 1, w2 + 1)
-                replace = edit(w1+1, w2)
-                
-                memo[(w1, w2)] = 1 + min(insert, delete, replace)
-                return memo[(w1, w2)]
-            
-            
-        memo = {}
-        minOps = edit(0,0)
         
-        return minOps
+        for j in range(len(dp[0])):
+            dp[0][j] = j
+            
+        for  i in range(len(dp)):
+            dp[i][0] = i
                 
+        
+        for i in range(1, len(dp)):
+            for j in range(1, len(dp[0])):
+                insert = delete = replace = 0
+                    
+                if word2[i-1] == word1[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                
+            
+                else:
+                    insert = dp[i - 1][j]
+                    delete = dp[i][j - 1]
+                    replace = dp[i-1][j-1]
+                    
+                    dp[i][j] += min(insert, delete, replace)
+                    
+        
+        return dp[len(word2)][len(word1)]
+                    
                 
         
         
