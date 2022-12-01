9f04d0b2e42a8da7c85d6b2dcac72d87984ff9eb
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-12-01 09:00:09 +0300

    Time: 2266 ms (5.01%), Space: 30.5 MB (27.61%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isPalindrome(self, s, l, r):
+        
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            
+            l += 1
+            r -= 1
+            
+        return True
+    
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        
+        def dfs(idx, part):
+            if idx >= len(s):
+                res.append(part)
+                return
+            
+            for i in range(idx, len(s)):
+                if self.isPalindrome(s, idx, i):
+                    curr = s[idx:i+1]
+                    dfs(i+1, part + [curr])
+                    
+        dfs(0,[])
+        
+        return res
+                    
+    
+                    
+                
+        
+        
