a6f5bcbf4e48883f16ec0a0954bc0ccda52031ac
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-12-01 17:34:47 +0300

    Time: 1182 ms (76.41%), Space: 44 MB (43.57%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -1,46 +1,56 @@
 class Solution:
     
-    @lru_cache(None)
-    def isPalindrome(self,s,l, r):
-        if l > r:
-            return True
-        
-        
-        if s[l] == s[r]:
-            return self.isPalindrome(s,l+1, r-1)
-            
-        return False
-                
-    
     def minCut(self, s: str) -> int:
-        if s == s[::-1]:
-            return 0
+        n = len(s)
+        dp = [[0 for _ in range(n)] for _ in range(n)]
         
+        for i in range(n-1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j]:
+                    # if the substring of size 2 and below or if the inner substsring is already a palindrome
+                    if j - i <= 1 or dp[i+1][j-1]:
+                        dp[i][j] = 1
+                        
+        main_dp = [-1 for _ in range(n+1)]
         
-        def dfs(idx):
-            if idx in memo:
-                return memo[idx]
-            
-            
-            if idx == len(s):
-                return -1
-            
+        for i in range(n-1, -1, -1):
             
             minCut = float('inf')
             
-            for i in range(idx, len(s)):
-                if self.isPalindrome(s,idx, i):
-                    minCut = min(minCut, 1 + dfs(i+1))
+            for j in range(i, n):
+                
+                if dp[i][j]:
+                    minCut = min(minCut, main_dp[j+1] + 1)
                     
-                    
-            memo[idx] = minCut
-            return memo[idx]
-                    
-                    
-        memo= {}
-        minCut = dfs(0)
+            main_dp[i] = minCut
+            
         
-        return minCut
+        return main_dp[0]
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+
+        
+        
         
         
         
