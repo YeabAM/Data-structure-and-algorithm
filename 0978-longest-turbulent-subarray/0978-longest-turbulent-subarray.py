919f5418ae8f6b357cb1600ccfc8d96b17d269ab
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-12-06 16:22:57 +0300

    Time: 1017 ms (62.99%), Space: 17.9 MB (75.14%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -14,24 +14,8 @@ class Solution:
                 if arr[i] != arr[i-1]:
                     last = "I" if arr[i] > arr[i - 1] else"D"
                     dp[i] += 1
-        maxx = max(dp)
-        
-        dp = [1] * len(arr)
-        
-        last = "D"
-        for i in range(1, len(arr)):
-            if arr[i] < arr[i - 1] and last == "I":
-                dp[i] += dp[i - 1]
-                last = "D"
-            elif arr[i] > arr[i - 1] and last == "D":
-                dp[i] += dp[i - 1]
-                last = "I"
-            else:
-                if arr[i] != arr[i-1]:
-                    last = "I" if arr[i] > arr[i - 1] else"D"
-                    dp[i] += 1
                     
-                
-        return max(maxx, max(dp))
+        maxx = max(dp)
+        return maxx
         
                 
