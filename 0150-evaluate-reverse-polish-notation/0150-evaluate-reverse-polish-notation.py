ebeec307d9eab3c29ba10ca4304aebd48b1b71b1
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-12-17 08:43:15 +0300

    Time: 72 ms (88.75%), Space: 14.4 MB (60.84%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,18 +1,32 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        i=0
-        ans=0
-        st=[]
-        for i in range(len(tokens)):
-            #print(st)
-            try:    # to find that the current is int or not
-                x=int(tokens[i])
-                st.append(x)
-            except:   # not int then
-                b=st.pop()
-                a=st.pop()
-                if tokens[i]=="/": st.append(int(a/b))
-                elif(tokens[i]=="*"):st.append(a*b)
-                elif(tokens[i]=="+"):st.append(a+b)
-                else:st.append(a-b)
-        return st.pop()
+        stack = []
+
+        for val in tokens:
+            if self.isOperator(val):
+                operand1 = stack.pop()
+                operand2 = stack.pop()
+
+                if val == '/':
+                    res = int(operand2 / operand1)
+                    stack.append(res)
+                
+                elif val == '+':
+                    res = operand2 + operand1
+                    stack.append(res)
+                
+                elif val == '-':
+                    res = operand2 - operand1
+                    stack.append(res)
+                
+                else:
+                    res = operand2 * operand1
+                    stack.append(res)
+            
+            else:
+                stack.append(int(val))
+        
+        return stack.pop()
+    
+    def isOperator(self, val):
+        return val == '+' or val == '-' or val == '*' or val == '/'
