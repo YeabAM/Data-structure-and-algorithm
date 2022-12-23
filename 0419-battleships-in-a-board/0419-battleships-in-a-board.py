dda078c8e07689a8063c74264a37241368a4f8b0
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2022-12-23 16:08:26 +0300

    Time: 193 ms (27.56%), Space: 14.5 MB (38.85%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -2,35 +2,18 @@ class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
         rows = len(board)
         cols = len(board[-1])
+        count = 0    
         
-        
-        visited = set()
-        count = 0
-        direction = [(0, 1), (0, -1),(1, 0),(-1, 0)]
         in_bound = lambda r, c: 0 <= r < rows and 0 <= c < cols
-        
-        def bfs(r, c):
-            
-            queue = deque()
-            queue.append((r, c))
-            visited.add((r, c))
-            
-            while queue:
-                r, c = queue.popleft()
-            
-                for dx, dy in direction:
-                    nr, nc = r + dx, c + dy
-
-                    if in_bound(nr, nc) and (nr, nc) not in visited and board[nr][nc] == 'X':
-                        visited.add((nr, nc))
-                        queue.append((nr, nc))
-                        
                         
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == 'X' and (r, c) not in visited:
-                    bfs(r, c)
-                    count += 1
+                if board[r][c] == 'X':
+                    
+                    if (not in_bound(r-1, c) or board[r-1][c] == '.') and\
+                    (not in_bound(r, c - 1) or board[r][c - 1] == '.'):
+                        
+                        count += 1
                     
         return count
                     
