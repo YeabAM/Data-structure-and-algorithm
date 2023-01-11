bef79e97571ff9735fb021ba4449340a0232b8c0
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2023-01-11 17:32:04 +0300

    Time: 3774 ms (5.08%), Space: 59.1 MB (16.93%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,17 +1,21 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [[0 for _ in range(amount+1)] for _ in range(len(coins) + 1)]
         
-        dp[0][0] = 1
+        @lru_cache(None)
+        def countSteps(idx, total):
+            if total == amount:
+                return 1
+            
+            if idx >= len(coins):
+                return 0
+            
+            count = 0
+            
+            for i in range(idx, len(coins)):
+                if total + coins[i] <= amount:
+                    count += countSteps(i, total + coins[i])
+            
+            return count
         
-        for r in range(1, len(dp)):
-            for c in range(len(dp[0])):
-                dp[r][c] = dp[r-1][c]
+        return countSteps(0, 0)
                 
-                prevSum = c - coins[r-1]
-                
-                if prevSum >= 0:
-                    dp[r][c] += dp[r][prevSum]
-                
-                    
-        return dp[len(coins)][amount]
