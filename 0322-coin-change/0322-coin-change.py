f12765ff4825b6225941d3671ec9e2992a27e0e6
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2023-01-11 15:37:55 +0300

    Time: 1609 ms (72.41%), Space: 37.9 MB (11.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,22 +1,30 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [inf for _ in range(amount + 1)]
-        dp[0] = 0
-        for i in range(1, len(dp)):
-            for j in range(len(coins)):
-                if i - coins[j] >= 0:
-                    # print('here')
-                    dp[i] = min(dp[i], dp[i-coins[j]] + 1)
-        # print(dp)        
-        return dp[-1] if dp[-1] != inf else -1
-                
+        
+        @lru_cache(None)
+        def countSteps(amount):
             
-        
-        
-        
+            # base case
+            if amount == 0:
+                return 0
+            # if less than 0, which means the subproblem fails
+            if amount < 0:
+                return -1
+            
+            res = float('inf')
+            for i in range(len(coins)):
+                # recursion, minus each recursion
+                subproblem = countSteps(amount - coins[i])
+                if subproblem == -1:
+                    continue
                     
+                res = min(res, subproblem+1)
+                
+            if res == float('inf'):
+                return -1
+            else:
+                return res
             
-                
-                
-            
-                
+        return countSteps(amount)
+    
+        
