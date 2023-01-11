5512ddeeb51a1dab25b3650a61dcfde5a3dc3fe2
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2023-01-11 14:34:27 +0300

    Time: 671 ms (94.87%), Space: 50.7 MB (78.21%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        graph = defaultdict(list)
+        
+        for v1, v2 in edges:
+            graph[v1].append(v2)
+            graph[v2].append(v1)
+            
+        def dfs(parent, node):
+            if not graph[node]:
+                return hasApple[node]
+            
+            currVariable = 0
+            # print(node, currVariable, "before")
+            for nei in graph[node]:
+                if nei != parent:
+                    currVariable += dfs(node, nei)
+                
+            
+            if (currVariable > 0 or hasApple[node]) and node != 0:
+                currVariable += 1
+                
+            # print(node, currVariable, 'after')
+        
+                    
+            return currVariable
+        
+        
+        total_variable = dfs(-1, 0)
+        # print(total_variable)
+        
+        return total_variable * 2
