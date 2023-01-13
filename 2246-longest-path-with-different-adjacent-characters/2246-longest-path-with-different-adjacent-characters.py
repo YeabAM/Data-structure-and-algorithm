b71922b09ee83318e9e0b5b6eca59a7585b74602
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2023-01-13 08:26:25 +0300

    Time: 1760 ms (88.06%), Space: 150 MB (72.27%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -1,23 +1,34 @@
 class Solution:
     def longestPath(self, parent: List[int], s: str) -> int:
-        longest_path = 1
-        n = len(parent)
-        # convert list to graph
         graph = defaultdict(list)
-        for index in range(1,n):
-            graph[parent[index]].append(index)
-
-        def dfs(currNode):
-            nonlocal longest_path
-            if not graph[currNode]:return 1
-            currLongestPath = 1
-            for child in graph[currNode]:
-                childLongestPath = dfs(child)
-                if s[currNode] != s[child]:
-                    longest_path = max(longest_path,childLongestPath+currLongestPath)
-                    currLongestPath = max(currLongestPath,childLongestPath+1)
-            return currLongestPath
+        ans = 1
+        
+        for node, parent in enumerate(parent):
+            graph[parent].append(node)
+            
+        
+        def dfs(node):
+            nonlocal ans
+            longest = secondLongest = 0
+            
+            for child in graph[node]:
+                currPath = dfs(child)
+            
+                if s[child] != s[node]:
+                    
+                    if currPath > longest:
+                        secondLongest = longest
+                        longest = currPath
+                        
+                    elif currPath > secondLongest:
+                        secondLongest = currPath
+                    
+            ans = max(ans, longest + secondLongest + 1)
+                    
+            return longest + 1
+            
         dfs(0)
-        return longest_path
+        
+        return ans
             
         
