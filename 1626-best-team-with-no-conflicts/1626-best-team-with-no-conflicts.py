e8b39c03edeb7db2e74cf5dc6c3b008b29a32a01
Yeabkalu Merkebe <52351316+YeabAM@users.noreply.github.com>
2023-02-01 10:24:56 +0300

    Time: 1716 ms (74.83%), Space: 14.3 MB (23.08%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,9 +1,12 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-        pairs = [[scores[i], ages[i]] for i in range(len(scores))]
+        pairs = []
+        for score, age in zip(scores, ages):
+            pairs.append((score, age))
+            
         pairs.sort()
         dp = [pairs[i][0] for i in range(len(pairs))]
-
+        
         for i in range(len(pairs)):
             max_score, max_age = pairs[i]
             for j in range(i):
